f631c3e1becfc2ee4b9199de0c37dbbb23e529f1
gameshler <jb.nullsolutions@outlook.com>
2025-01-19 00:01:08 +0200

    Ref: Installers

--- 4 Installers/1 Installers.ps1
+++ 4 Installers/1 Installers.ps1
@@ -42,23 +42,17 @@ function show-menu {
     Write-Host "Optimize your game settings to achieve this." -ForegroundColor Green
     Write-Host "Further tuning can be done via config files or launch options." -ForegroundColor Green
     Write-Host ""
-    Write-Host " 1. Exit" -ForegroundColor Red
-    Write-Host " 2. 7-Zip" -ForegroundColor Cyan
-    Write-Host " 3. Battle.net" -ForegroundColor Cyan
-    Write-Host " 4. Discord" -ForegroundColor Cyan
-    Write-Host " 5. Electronic Arts" -ForegroundColor Cyan
-    Write-Host " 6. Epic Games" -ForegroundColor Cyan
-    Write-Host " 7. Escape From Tarkov" -ForegroundColor Cyan
-    Write-Host " 8. GOG launcher" -ForegroundColor Cyan
-    Write-Host " 9. Browsers" -ForegroundColor Cyan
-    Write-Host "10. League Of Legends" -ForegroundColor Cyan
-    Write-Host "11. Notepad ++" -ForegroundColor Cyan
-    Write-Host "12. OBS Studio" -ForegroundColor Cyan
-    Write-Host "13. Roblox" -ForegroundColor Cyan
-    Write-Host "14. Rockstar Games" -ForegroundColor Cyan
-    Write-Host "15. Steam" -ForegroundColor Cyan
-    Write-Host "16. Ubisoft Connect" -ForegroundColor Cyan
-    Write-Host "17. Valorant" -ForegroundColor Cyan
+    Write-Host "1. Exit" -ForegroundColor Red
+    Write-Host "2. 7-Zip" -ForegroundColor Cyan
+    Write-Host "3. Game Launchers" -ForegroundColor Cyan
+    Write-Host "4. Discord" -ForegroundColor Cyan
+    Write-Host "5. Escape From Tarkov" -ForegroundColor Cyan
+    Write-Host "6. Browsers" -ForegroundColor Cyan
+    Write-Host "7. League Of Legends" -ForegroundColor Cyan
+    Write-Host "8. Notepad ++" -ForegroundColor Cyan
+    Write-Host "9. OBS Studio" -ForegroundColor Cyan
+    Write-Host "10.Roblox" -ForegroundColor Cyan
+    Write-Host "11.Valorant" -ForegroundColor Cyan
    
 }
 
@@ -73,17 +67,30 @@ function show-browser-menu {
     Write-Host "4. Thorium Browser" -ForegroundColor Cyan
 }
 
+function show-launchers-menu {
+    Clear-Host
+    Write-Host "Choose a game launcher to install:" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "1. Exit" -ForegroundColor Red
+    Write-Host "2. Battle.net" -ForegroundColor Cyan
+    Write-Host "3. Steam" -ForegroundColor Cyan
+    Write-Host "4. Epic Games" -ForegroundColor Cyan
+    Write-Host "5. Ubisoft Connect" -ForegroundColor Cyan
+    Write-Host "6. Electronic Arts" -ForegroundColor Cyan
+    Write-Host "7. Rockstar Games" -ForegroundColor Cyan
+    Write-Host "8. GOG launcher" -ForegroundColor Cyan
+
+}
+
 show-menu
 
 while ($true) {
     $choice = Read-Host " "
-    if ($choice -match '^(1[0-7]|[1-9])$' ) {
+    if ($choice -match '^(1[0-1]|[1-9])$' ) {
         switch ($choice ) { 
             1 {
-
                 Clear-Host
                 exit
-
             }
             2 {
 
@@ -97,20 +104,88 @@ while ($true) {
 
             }
             3 {
-
-                Clear-Host
-                Write-Host "Installing: Battle.net . . ."
-                # download battle.net
-                Get-FileFromWeb -URL "https://downloader.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe" -File "$env:TEMP\Battle.net.exe"
-                # install battle.net 
-                Start-Process "$env:TEMP\Battle.net.exe" -ArgumentList '--lang=enUS --installpath="C:\Program Files (x86)\Battle.net"'
-                # create battle.net shortcut
-                $WshShell = New-Object -comObject WScript.Shell
-                $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Battle.net.lnk")
-                $Shortcut.TargetPath = "$env:SystemDrive\Program Files (x86)\Battle.net\Battle.net Launcher.exe"
-                $Shortcut.Save()
-                show-menu
-
+                show-launchers-menu
+                $launcherChoice = Read-Host " "
+                if ($launcherChoice -match '^[1-8]$' ) {
+                    switch ($launcherChoice ) { 
+                        1 {
+                            Clear-Host
+                            show-menu
+                        }
+                        2 {
+                            Clear-Host
+                            Write-Host "Installing: Battle.net . . ."
+                            # download battle.net
+                            Get-FileFromWeb -URL "https://downloader.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe" -File "$env:TEMP\Battle.net.exe"
+                            # install battle.net 
+                            Start-Process "$env:TEMP\Battle.net.exe" -ArgumentList '--lang=enUS --installpath="C:\Program Files (x86)\Battle.net"'
+                            # create battle.net shortcut
+                            $WshShell = New-Object -comObject WScript.Shell
+                            $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\Battle.net.lnk")
+                            $Shortcut.TargetPath = "$env:SystemDrive\Program Files (x86)\Battle.net\Battle.net Launcher.exe"
+                            $Shortcut.Save()
+                            show-launchers-menu
+                        }
+                        3 {
+                            Clear-Host
+                            Write-Host "Installing: Steam . . ."
+                            # download steam
+                            Get-FileFromWeb -URL "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -File "$env:TEMP\Steam.exe"
+                            # install steam
+                            Start-Process -wait "$env:TEMP\Steam.exe" -ArgumentList "/S"
+                            show-launchers-menu
+                        }
+                        4 {
+                            Clear-Host
+                            Write-Host "Installing: Epic Games . . ."
+                            # download epic games
+                            Get-FileFromWeb -URL "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Win32/EpicInstaller-15.17.1.msi?launcherfilename=EpicInstaller-15.17.1.msi" -File "$env:TEMP\Epic Games.msi"
+                            # install epic games
+                            Start-Process -wait "$env:TEMP\Epic Games.msi" -ArgumentList "/quiet"
+                            show-launchers-menu
+                        }
+                        5 {
+                            Clear-Host
+                            Write-Host "Installing: Ubisoft Connect . . ."
+                            # download ubisoft connect
+                            Get-FileFromWeb -URL "https://static3.cdn.ubi.com/orbit/launcher_installer/UbisoftConnectInstaller.exe" -File "$env:TEMP\Ubisoft Connect.exe"
+                            # install ubisoft connect
+                            Start-Process -wait "$env:TEMP\Ubisoft Connect.exe" -ArgumentList "/S"
+                            show-launchers-menu
+                        }
+                        6 {
+                            Clear-Host
+                            Write-Host "Installing: Electronic Arts . . ."
+                            # download electronic arts
+                            Get-FileFromWeb -URL "https://origin-a.akamaihd.net/EA-Desktop-Client-Download/installer-releases/EAappInstaller.exe" -File "$env:TEMP\Electronic Arts.exe"
+                            # install electronic arts
+                            Start-Process "$env:TEMP\Electronic Arts.exe"
+                            show-launchers-menu
+                        }
+                        7 {
+                            Clear-Host
+                            Write-Host "Installing: Rockstar Games . . ."
+                            # download rockstar games
+                            Get-FileFromWeb -URL "https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe" -File "$env:TEMP\Rockstar Games.exe"
+                            # install rockstar games
+                            Start-Process "$env:TEMP\Rockstar Games.exe"
+                            show-launchers-menu
+                        }
+                        8 {
+                            Clear-Host
+                            Write-Host "Installing: GOG launcher . . ."
+                            # download gog launcher
+                            Get-FileFromWeb -URL "https://webinstallers.gog-statics.com/download/GOG_Galaxy_2.0.exe" -File "$env:TEMP\GOG launcher.exe"
+                            # install gog launcher
+                            Start-Process "$env:TEMP\GOG launcher.exe"
+                            show-launchers-menu
+                        }
+                    }
+                }
+                else {
+                    Write-Host "Invalid input. Please select a valid option (1-8)."
+                }
+               
             }
             4 {
 
@@ -123,29 +198,9 @@ while ($true) {
                 show-menu
 
             }
+            
+            
             5 {
-
-                Clear-Host
-                Write-Host "Installing: Electronic Arts . . ."
-                # download electronic arts
-                Get-FileFromWeb -URL "https://origin-a.akamaihd.net/EA-Desktop-Client-Download/installer-releases/EAappInstaller.exe" -File "$env:TEMP\Electronic Arts.exe"
-                # install electronic arts
-                Start-Process "$env:TEMP\Electronic Arts.exe"
-                show-menu
-
-            }
-            6 {
-
-                Clear-Host
-                Write-Host "Installing: Epic Games . . ."
-                # download epic games
-                Get-FileFromWeb -URL "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Win32/EpicInstaller-15.17.1.msi?launcherfilename=EpicInstaller-15.17.1.msi" -File "$env:TEMP\Epic Games.msi"
-                # install epic games
-                Start-Process -wait "$env:TEMP\Epic Games.msi" -ArgumentList "/quiet"
-                show-menu
-
-            }
-            7 {
 
                 Clear-Host
                 Write-Host "Installing: Escape From Tarkov . . ."
@@ -156,20 +211,39 @@ while ($true) {
                 show-menu
 
             }
-            8 {
-
-                Clear-Host
-                Write-Host "Installing: GOG launcher . . ."
-                # download gog launcher
-                Get-FileFromWeb -URL "https://webinstallers.gog-statics.com/download/GOG_Galaxy_2.0.exe" -File "$env:TEMP\GOG launcher.exe"
-                # install gog launcher
-                Start-Process "$env:TEMP\GOG launcher.exe"
-                show-menu
-
-            }
-            9 {
+            
+            6 {
                 show-browser-menu
                 $browserChoice = Read-Host " "
+                $firefoxExtensions = @(
+                    "https://addons.mozilla.org/en-US/firefox/addon/decentraleyes/", 
+                    "https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/", 
+                    "https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/",
+                    "https://addons.mozilla.org/en-US/firefox/addon/react-devtools/", 
+                    "https://addons.mozilla.org/en-US/firefox/addon/search_by_image/"
+                )
+                $chromiumExtensions = @(
+                    "https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm", 
+                    "https://chromewebstore.google.com/detail/privacy-badger/pkehgijcmpdhfbdbbnkijodmdjhbjlgp", 
+                    "https://chromewebstore.google.com/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi", 
+                    "https://chromewebstore.google.com/detail/search-by-image/cnojnbdhbhnkbcieeekonklommdnndci", 
+                    "https://chromewebstore.google.com/detail/decentraleyes/ldpochfccmkkmhdbclfhpagapcfdljkj"
+                )
+
+                $thoriumPaths = @(
+                    "$env:LOCALAPPDATA\Thorium\Application\thorium.exe"
+                )
+
+                $chromePaths = @(
+                    "C:\Program Files\Google\Chrome\Application\chrome.exe", 
+                    "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+                    
+                )
+
+                $firefoxPaths = @(
+                    "C:\Program Files\Mozilla Firefox\firefox.exe", 
+                    "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" 
+                )
                 if ($browserChoice -match '^[1-4]$') {
                     switch ($browserChoice) {
                         1 {
@@ -183,8 +257,12 @@ while ($true) {
                             Get-FileFromWeb -URL "https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi" -File "$env:TEMP\Chrome.msi"
                             # install google chrome
                             Start-Process -wait "$env:TEMP\Chrome.msi" -ArgumentList "/quiet"
-                            # open ublock origin in web browser
-                            Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" "https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm"
+                            $chromePath = $chromePaths | Where-Object { Test-Path $_ } | Select-Object -First 1
+                            if ($chromePath) {
+                                foreach ($extension in $chromiumExtensions) {
+                                    Start-Process $chromePath $extension
+                                }
+                            }
                             show-menu
                         } 
                         3 {
@@ -195,16 +273,11 @@ while ($true) {
                             # install firefox
                             Start-Process -wait "$env:TEMP\Firefox Installer.exe" -ArgumentList "/S"
                 
-                            $extensions = @(
-                                "https://addons.mozilla.org/en-US/firefox/addon/decentraleyes/", 
-                                "https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/", 
-                                "https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/",
-                                "https://addons.mozilla.org/en-US/firefox/addon/react-devtools/", 
-                                "https://addons.mozilla.org/en-US/firefox/addon/search_by_image/"
-            
-                            )
-                            foreach ($extension in $extensions) {
-                                Start-Process "C:\Program Files\Mozilla Firefox\firefox.exe" $extension
+                            $firefoxPath = $firefoxPaths | Where-Object { Test-Path $_ } | Select-Object -First 1
+                            if ($firefoxPath) {
+                                foreach ($extension in $firefoxExtensions) {
+                                    Start-Process $firefoxPath $extension
+                                }
                             }
 
                             show-menu
@@ -216,6 +289,12 @@ while ($true) {
                             Get-FileFromWeb -URL "https://github.com/Alex313031/Thorium-Win/releases/download/M128.0.6613.189/thorium_AVX2_mini_installer.exe" -File "$env:TEMP\Thorium Browser.exe"
                             # install thorium browser
                             Start-Process -wait "$env:TEMP\Thorium Browser.exe" -ArgumentList "/S"
+                            $thoriumPath = $thoriumPaths | Where-Object { Test-Path $_ } | Select-Object -First 1
+                            if ($thoriumPath) {
+                                foreach ($extension in $chromiumExtensions) {
+                                    Start-Process $thoriumPath $extension
+                                }
+                            }
                             show-menu
                         }
                     }
@@ -225,7 +304,7 @@ while ($true) {
                 }
 
             }
-            10 {
+            7 {
 
                 Clear-Host
                 Write-Host "Installing: League Of Legends . . ."
@@ -236,7 +315,7 @@ while ($true) {
                 show-menu
 
             }
-            11 {
+            8 {
 
                 Clear-Host
                 Write-Host "Installing: Notepad ++ . . ."
@@ -247,7 +326,7 @@ while ($true) {
                 show-menu
 
             }
-            12 {
+            9 {
 
                 Clear-Host
                 Write-Host "Installing: OBS Studio . . ."
@@ -258,7 +337,7 @@ while ($true) {
                 show-menu
 
             }
-            13 {
+            10 {
 
                 Clear-Host
                 Write-Host "Installing: Roblox . . ."
@@ -269,40 +348,8 @@ while ($true) {
                 show-menu
 
             }
-            14 {
-
-                Clear-Host
-                Write-Host "Installing: Rockstar Games . . ."
-                # download rockstar games
-                Get-FileFromWeb -URL "https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe" -File "$env:TEMP\Rockstar Games.exe"
-                # install rockstar games
-                Start-Process "$env:TEMP\Rockstar Games.exe"
-                show-menu
-
-            }
-            15 {
-
-                Clear-Host
-                Write-Host "Installing: Steam . . ."
-                # download steam
-                Get-FileFromWeb -URL "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -File "$env:TEMP\Steam.exe"
-                # install steam
-                Start-Process -wait "$env:TEMP\Steam.exe" -ArgumentList "/S"
-                show-menu
-
-            }
-            16 {
-
-                Clear-Host
-                Write-Host "Installing: Ubisoft Connect . . ."
-                # download ubisoft connect
-                Get-FileFromWeb -URL "https://static3.cdn.ubi.com/orbit/launcher_installer/UbisoftConnectInstaller.exe" -File "$env:TEMP\Ubisoft Connect.exe"
-                # install ubisoft connect
-                Start-Process -wait "$env:TEMP\Ubisoft Connect.exe" -ArgumentList "/S"
-                show-menu
-
-            }
-            17 {
+            
+            11 {
 
                 Clear-Host
                 Write-Host "Installing: Valorant . . ."
@@ -316,5 +363,5 @@ while ($true) {
            
         } 
     }
-    else { Write-Host "Invalid input. Please select a valid option (1-17)." } 
+    else { Write-Host "Invalid input. Please select a valid option (1-11)." } 
 }
