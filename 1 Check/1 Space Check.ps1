c23a91a0226761c50090dce6079f8f6082111a80
FR33THY <fr33thy@gmail.com>
2026-03-27 19:37:03 +1100

    Upload

--- 1 Check/1 Space Check.ps1
+++ 1 Check/1 Space Check.ps1
@@ -12,8 +12,10 @@ Write-Host "Keep SSD's at least 10% free`n"
 
 # show space for all drives
 Get-Volume | Where-Object {$_.DriveLetter} | Sort-Object DriveLetter | ForEach-Object {
+try {
 $percentRemain = ($_.SizeRemaining / $_.Size) * 100
 Write-Host "$($_.DriveLetter): Free space = $($percentRemain.ToString().substring(0,4))%"
+} catch {}
 }
 
 # open file explorer
