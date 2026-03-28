e12a3c5dc466858dade78d99beb8247eaf1ade66
FR33THY <fr33thy@gmail.com>
2026-03-28 12:55:04 +1100

    Upload

---
+++ 4 Installers/5 PBO Tuner.ps1
@@ -0,0 +1,89 @@
+        # SCRIPT RUN AS ADMIN
+        If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+        {Start-Process PowerShell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+        Exit}
+        $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + " (Administrator)"
+        $Host.UI.RawUI.BackgroundColor = "Black"
+        $Host.PrivateData.ProgressBackgroundColor = "Black"
+        $Host.PrivateData.ProgressForegroundColor = "White"
+        Clear-Host
+
+        # SCRIPT CHECK INTERNET
+        if (!(Test-Connection -ComputerName "8.8.8.8" -Count 1 -Quiet -ErrorAction SilentlyContinue)) {
+        Write-Host "Internet Connection Required`n" -ForegroundColor Red
+        Pause
+        exit
+        }
+
+        # SCRIPT SILENT
+        $progresspreference = 'silentlycontinue'
+
+        # FUNCTION FASTER DOWNLOADS % BAR
+        function Get-FileFromWeb {
+        param ([Parameter(Mandatory)][string]$URL, [Parameter(Mandatory)][string]$File)
+        function Show-Progress {
+        param ([Parameter(Mandatory)][Single]$TotalValue, [Parameter(Mandatory)][Single]$CurrentValue, [Parameter(Mandatory)][string]$ProgressText, [Parameter()][int]$BarSize = 10, [Parameter()][switch]$Complete)
+        $percent = $CurrentValue / $TotalValue
+        $percentComplete = $percent * 100
+        if ($psISE) { Write-Progress "$ProgressText" -id 0 -percentComplete $percentComplete }
+        else { Write-Host -NoNewLine "`r$ProgressText $(''.PadRight($BarSize * $percent, [char]9608).PadRight($BarSize, [char]9617)) $($percentComplete.ToString('##0.00').PadLeft(6)) % " }
+        }
+        try {
+        $request = [System.Net.HttpWebRequest]::Create($URL)
+        $response = $request.GetResponse()
+        if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) { throw "401, 403 or 404 '$URL'." }
+        if ($File -match '^\.\\') { $File = Join-Path (Get-Location -PSProvider 'FileSystem') ($File -Split '^\.')[1] }
+        if ($File -and !(Split-Path $File)) { $File = Join-Path (Get-Location -PSProvider 'FileSystem') $File }
+        if ($File) { $fileDirectory = $([System.IO.Path]::GetDirectoryName($File)); if (!(Test-Path($fileDirectory))) { [System.IO.Directory]::CreateDirectory($fileDirectory) | Out-Null } }
+        [long]$fullSize = $response.ContentLength
+        [byte[]]$buffer = new-object byte[] 1048576
+        [long]$total = [long]$count = 0
+        $reader = $response.GetResponseStream()
+        $writer = new-object System.IO.FileStream $File, 'Create'
+        do {
+        $count = $reader.Read($buffer, 0, $buffer.Length)
+        $writer.Write($buffer, 0, $count)
+        $total += $count
+        if ($fullSize -gt 0) { Show-Progress -TotalValue $fullSize -CurrentValue $total -ProgressText " $($File.Name)" }
+        } while ($count -gt 0)
+        }
+        finally {
+        $reader.Close()
+        $writer.Close()
+        }
+        }
+
+## explorer "https://www.overclock.net/threads/corecycler-tool-for-testing-single-core-stability-e-g-curve-optimizer-settings.1777398/page-45#post-28999750"
+Write-Host "Installing: PBO2 Tuner..."
+Write-Host "AM4 CPUS only, 5000 series and below`n" -ForegroundColor Red
+
+Pause
+
+# download pbo2 tuner
+Get-FileFromWeb -URL "https://github.com/FR33THYFR33THY/files/raw/refs/heads/main/PBO2%20Tuner.zip" -File "$env:SystemRoot\Temp\PBO2 Tuner.zip"
+
+# extract file
+Expand-Archive -Path "$env:SystemRoot\Temp\PBO2 Tuner.zip" -DestinationPath "$env:SystemDrive\Program Files (x86)\PBO2 Tuner" -Force
+
+# create desktop shortcut
+$WshShell = New-Object -comObject WScript.Shell
+$Desktop = (New-Object -ComObject Shell.Application).Namespace('shell:Desktop').Self.Path
+$Shortcut = $WshShell.CreateShortcut("$Desktop\PBO2 Tuner.lnk")
+$Shortcut.TargetPath = "$env:SystemDrive\Program Files (x86)\PBO2 Tuner\PBO2 Tuner.exe"
+$Shortcut.WorkingDirectory = "$env:SystemDrive\Program Files (x86)\PBO2 Tuner"
+$Shortcut.Save()
+
+# create start menu shortcut
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\PBO2 Tuner.lnk")
+$Shortcut.TargetPath = "$env:SystemDrive\Program Files (x86)\PBO2 Tuner\PBO2 Tuner.exe"
+$Shortcut.WorkingDirectory = "$env:SystemDrive\Program Files (x86)\PBO2 Tuner"
+$Shortcut.Save()
+
+# create pbo2 tuner (-10 undervolt startup) shortcut
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\PBO2 Tuner (-10 UNDERVOLT STARTUP).lnk")
+$Shortcut.TargetPath = "$env:SystemDrive\Program Files (x86)\PBO2 Tuner\PBO2 Tuner.exe"
+$Shortcut.WorkingDirectory = "$env:SystemDrive\Program Files (x86)\PBO2 Tuner"
+$Shortcut.Arguments = "-10 -10 -10 -10 -10 -10 -10 -10"
+$Shortcut.Save()
