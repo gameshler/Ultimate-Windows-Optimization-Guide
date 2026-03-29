38ca45c87046c5b53a82a593d16f16af7a93fb80
FR33THY <fr33thy@gmail.com>
2026-03-29 16:54:38 +1100

    Upload

--- 4 Installers/5 PBO Tuner.ps1
+++ 4 Installers/5 PBO Tuner.ps1
@@ -18,41 +18,6 @@
         # SCRIPT SILENT
         $progresspreference = 'silentlycontinue'
 
-        # FUNCTION FASTER DOWNLOADS % BAR
-        function Get-FileFromWeb {
-        param ([Parameter(Mandatory)][string]$URL, [Parameter(Mandatory)][string]$File)
-        function Show-Progress {
-        param ([Parameter(Mandatory)][Single]$TotalValue, [Parameter(Mandatory)][Single]$CurrentValue, [Parameter(Mandatory)][string]$ProgressText, [Parameter()][int]$BarSize = 10, [Parameter()][switch]$Complete)
-        $percent = $CurrentValue / $TotalValue
-        $percentComplete = $percent * 100
-        if ($psISE) { Write-Progress "$ProgressText" -id 0 -percentComplete $percentComplete }
-        else { Write-Host -NoNewLine "`r$ProgressText $(''.PadRight($BarSize * $percent, [char]9608).PadRight($BarSize, [char]9617)) $($percentComplete.ToString('##0.00').PadLeft(6)) % " }
-        }
-        try {
-        $request = [System.Net.HttpWebRequest]::Create($URL)
-        $response = $request.GetResponse()
-        if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) { throw "401, 403 or 404 '$URL'." }
-        if ($File -match '^\.\\') { $File = Join-Path (Get-Location -PSProvider 'FileSystem') ($File -Split '^\.')[1] }
-        if ($File -and !(Split-Path $File)) { $File = Join-Path (Get-Location -PSProvider 'FileSystem') $File }
-        if ($File) { $fileDirectory = $([System.IO.Path]::GetDirectoryName($File)); if (!(Test-Path($fileDirectory))) { [System.IO.Directory]::CreateDirectory($fileDirectory) | Out-Null } }
-        [long]$fullSize = $response.ContentLength
-        [byte[]]$buffer = new-object byte[] 1048576
-        [long]$total = [long]$count = 0
-        $reader = $response.GetResponseStream()
-        $writer = new-object System.IO.FileStream $File, 'Create'
-        do {
-        $count = $reader.Read($buffer, 0, $buffer.Length)
-        $writer.Write($buffer, 0, $count)
-        $total += $count
-        if ($fullSize -gt 0) { Show-Progress -TotalValue $fullSize -CurrentValue $total -ProgressText " $($File.Name)" }
-        } while ($count -gt 0)
-        }
-        finally {
-        $reader.Close()
-        $writer.Close()
-        }
-        }
-
 ## explorer "https://www.overclock.net/threads/corecycler-tool-for-testing-single-core-stability-e-g-curve-optimizer-settings.1777398/page-45#post-28999750"
 Write-Host "Installing: PBO2 Tuner..."
 Write-Host "AM4 CPUS only, 5000 series and below`n" -ForegroundColor Red
@@ -60,7 +25,7 @@ Write-Host "AM4 CPUS only, 5000 series and below`n" -ForegroundColor Red
 Pause
 
 # download pbo2 tuner
-Get-FileFromWeb -URL "https://github.com/FR33THYFR33THY/files/raw/refs/heads/main/PBO2%20Tuner.zip" -File "$env:SystemRoot\Temp\PBO2 Tuner.zip"
+iwr "https://github.com/FR33THYFR33THY/files/raw/refs/heads/main/PBO2%20Tuner.zip" -OutFile "$env:SystemRoot\Temp\PBO2 Tuner.zip"
 
 # extract file
 Expand-Archive -Path "$env:SystemRoot\Temp\PBO2 Tuner.zip" -DestinationPath "$env:SystemDrive\Program Files (x86)\PBO2 Tuner" -Force
