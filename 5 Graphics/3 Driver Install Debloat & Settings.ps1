bc476a967e1e8dd9a870ee5d30314a93745f5a9c
FR33THY <fr33thy@gmail.com>
2026-03-31 12:18:00 +1100

    Update

--- 5 Graphics/3 Driver Install Debloat & Settings.ps1
+++ 5 Graphics/3 Driver Install Debloat & Settings.ps1
@@ -18,60 +18,6 @@
         # SCRIPT SILENT
         $progresspreference = 'silentlycontinue'
 
-		# FUNCTION MODERN FILE PICKER
-    	function Show-ModernFilePicker {
-    	param(
-    	[ValidateSet('Folder', 'File')]
-    	$Mode,
-    	[string]$fileType
-    	)
-    	if ($Mode -eq 'Folder') {
-    	$Title = 'Select Folder'
-    	$modeOption = $false
-    	$Filter = "Folders|`n"
-    	}
-    	else {
-    	$Title = 'Select File'
-    	$modeOption = $true
-    	if ($fileType) {
-    	$Filter = "$fileType Files (*.$fileType) | *.$fileType|All files (*.*)|*.*"
-    	}
-    	else {
-    	$Filter = 'All Files (*.*)|*.*'
-    	}
-    	}
-    	$AssemblyFullName = 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
-    	$Assembly = [System.Reflection.Assembly]::Load($AssemblyFullName)
-    	$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-    	$OpenFileDialog.AddExtension = $modeOption
-    	$OpenFileDialog.CheckFileExists = $modeOption
-    	$OpenFileDialog.DereferenceLinks = $true
-    	$OpenFileDialog.Filter = $Filter
-    	$OpenFileDialog.Multiselect = $false
-    	$OpenFileDialog.Title = $Title
-    	$OpenFileDialog.InitialDirectory = [Environment]::GetFolderPath('Desktop')
-    	$OpenFileDialogType = $OpenFileDialog.GetType()
-    	$FileDialogInterfaceType = $Assembly.GetType('System.Windows.Forms.FileDialogNative+IFileDialog')
-    	$IFileDialog = $OpenFileDialogType.GetMethod('CreateVistaDialog', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($OpenFileDialog, $null)
-    	$null = $OpenFileDialogType.GetMethod('OnBeforeVistaDialog', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($OpenFileDialog, $IFileDialog)
-    	if ($Mode -eq 'Folder') {
-    	[uint32]$PickFoldersOption = $Assembly.GetType('System.Windows.Forms.FileDialogNative+FOS').GetField('FOS_PICKFOLDERS').GetValue($null)
-    	$FolderOptions = $OpenFileDialogType.GetMethod('get_Options', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($OpenFileDialog, $null) -bor $PickFoldersOption
-    	$null = $FileDialogInterfaceType.GetMethod('SetOptions', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $FolderOptions)
-    	}
-    	$VistaDialogEvent = [System.Activator]::CreateInstance($AssemblyFullName, 'System.Windows.Forms.FileDialog+VistaDialogEvents', $false, 0, $null, $OpenFileDialog, $null, $null).Unwrap()
-    	[uint32]$AdviceCookie = 0
-    	$AdvisoryParameters = @($VistaDialogEvent, $AdviceCookie)
-    	$AdviseResult = $FileDialogInterfaceType.GetMethod('Advise', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $AdvisoryParameters)
-    	$AdviceCookie = $AdvisoryParameters[1]
-    	$Result = $FileDialogInterfaceType.GetMethod('Show', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, [System.IntPtr]::Zero)
-    	$null = $FileDialogInterfaceType.GetMethod('Unadvise', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $AdviceCookie)
-    	if ($Result -eq [System.Windows.Forms.DialogResult]::OK) {
-    	$FileDialogInterfaceType.GetMethod('GetResult', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $null)
-    	}
-    	return $OpenFileDialog.FileName
-    	}
-
 # download 7zip
 IWR "https://github.com/FR33THYFR33THY/files/raw/refs/heads/main/7%20Zip.exe" -OutFile "$env:SystemRoot\Temp\7 Zip.exe"
 
@@ -119,7 +65,11 @@ Clear-Host
 
 # select driver
 Start-Sleep -Seconds 5
-$InstallFile = Show-ModernFilePicker -Mode File
+Add-Type -AssemblyName System.Windows.Forms
+$Dialog = New-Object System.Windows.Forms.OpenFileDialog
+$Dialog.Filter = "All Files (*.*)|*.*"
+$Dialog.ShowDialog() | Out-Null
+$InstallFile = $Dialog.FileName
 
         Write-Host "DEBLOATING DRIVER`n"
 
@@ -446,7 +396,11 @@ Clear-Host
 
 # select driver
 Start-Sleep -Seconds 5
-$InstallFile = Show-ModernFilePicker -Mode File
+Add-Type -AssemblyName System.Windows.Forms
+$Dialog = New-Object System.Windows.Forms.OpenFileDialog
+$Dialog.Filter = "All Files (*.*)|*.*"
+$Dialog.ShowDialog() | Out-Null
+$InstallFile = $Dialog.FileName
 
         Write-Host "DEBLOATING DRIVER`n"
 
@@ -653,7 +607,11 @@ Clear-Host
 
 # select driver
 Start-Sleep -Seconds 5
-$InstallFile = Show-ModernFilePicker -Mode File
+Add-Type -AssemblyName System.Windows.Forms
+$Dialog = New-Object System.Windows.Forms.OpenFileDialog
+$Dialog.Filter = "All Files (*.*)|*.*"
+$Dialog.ShowDialog() | Out-Null
+$InstallFile = $Dialog.FileName
 
         Write-Host "DEBLOATING DRIVER`n"
 
--- 8 Advanced/9 Core 1 Thread 1.ps1
+++ 8 Advanced/9 Core 1 Thread 1.ps1
@@ -8,60 +8,6 @@
         $Host.PrivateData.ProgressForegroundColor = "White"
         Clear-Host
 
-		# FUNCTION MODERN FILE PICKER
-    	function Show-ModernFilePicker {
-    	param(
-    	[ValidateSet('Folder', 'File')]
-    	$Mode,
-    	[string]$fileType
-    	)
-    	if ($Mode -eq 'Folder') {
-    	$Title = 'Select Folder'
-    	$modeOption = $false
-    	$Filter = "Folders|`n"
-    	}
-    	else {
-    	$Title = 'Select File'
-    	$modeOption = $true
-    	if ($fileType) {
-    	$Filter = "$fileType Files (*.$fileType) | *.$fileType|All files (*.*)|*.*"
-    	}
-    	else {
-    	$Filter = 'All Files (*.*)|*.*'
-    	}
-    	}
-    	$AssemblyFullName = 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
-    	$Assembly = [System.Reflection.Assembly]::Load($AssemblyFullName)
-    	$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-    	$OpenFileDialog.AddExtension = $modeOption
-    	$OpenFileDialog.CheckFileExists = $modeOption
-    	$OpenFileDialog.DereferenceLinks = $true
-    	$OpenFileDialog.Filter = $Filter
-    	$OpenFileDialog.Multiselect = $false
-    	$OpenFileDialog.Title = $Title
-    	$OpenFileDialog.InitialDirectory = [Environment]::GetFolderPath('Desktop')
-    	$OpenFileDialogType = $OpenFileDialog.GetType()
-    	$FileDialogInterfaceType = $Assembly.GetType('System.Windows.Forms.FileDialogNative+IFileDialog')
-    	$IFileDialog = $OpenFileDialogType.GetMethod('CreateVistaDialog', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($OpenFileDialog, $null)
-    	$null = $OpenFileDialogType.GetMethod('OnBeforeVistaDialog', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($OpenFileDialog, $IFileDialog)
-    	if ($Mode -eq 'Folder') {
-    	[uint32]$PickFoldersOption = $Assembly.GetType('System.Windows.Forms.FileDialogNative+FOS').GetField('FOS_PICKFOLDERS').GetValue($null)
-    	$FolderOptions = $OpenFileDialogType.GetMethod('get_Options', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($OpenFileDialog, $null) -bor $PickFoldersOption
-    	$null = $FileDialogInterfaceType.GetMethod('SetOptions', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $FolderOptions)
-    	}
-    	$VistaDialogEvent = [System.Activator]::CreateInstance($AssemblyFullName, 'System.Windows.Forms.FileDialog+VistaDialogEvents', $false, 0, $null, $OpenFileDialog, $null, $null).Unwrap()
-    	[uint32]$AdviceCookie = 0
-    	$AdvisoryParameters = @($VistaDialogEvent, $AdviceCookie)
-    	$AdviseResult = $FileDialogInterfaceType.GetMethod('Advise', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $AdvisoryParameters)
-    	$AdviceCookie = $AdvisoryParameters[1]
-    	$Result = $FileDialogInterfaceType.GetMethod('Show', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, [System.IntPtr]::Zero)
-    	$null = $FileDialogInterfaceType.GetMethod('Unadvise', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $AdviceCookie)
-    	if ($Result -eq [System.Windows.Forms.DialogResult]::OK) {
-    	$FileDialogInterfaceType.GetMethod('GetResult', @('NonPublic', 'Public', 'Static', 'Instance')).Invoke($IFileDialog, $null)
-    	}
-    	return $OpenFileDialog.FileName
-    	}
-
 		Write-Host "TEMPORARILY DISABLE CPU CORE 1 & THREAD 1 FOR TESTING PER APP/GAME`n"
         Write-Host "CORE 1 THREAD 1:"
         Write-Host "1. Off: Already Running"
@@ -125,7 +71,11 @@ $hexadecimal = "{0:X}" -f $affinity
 
 # select game launcher lnk or exe
 Write-Host "SELECT LAUNCHER/GAME/SHORTCUT/EXE:"
-$gamelauncher = Show-ModernFilePicker -Mode File
+Add-Type -AssemblyName System.Windows.Forms
+$Dialog = New-Object System.Windows.Forms.OpenFileDialog
+$Dialog.Filter = "All Files (*.*)|*.*"
+$Dialog.ShowDialog() | Out-Null
+$gamelauncher = $Dialog.FileName
 
 Clear-Host
 
